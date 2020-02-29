0a9044875790adfe7b5b6d370fd080f6424fe236
Hubert <hubertle43100@gmail.com>
2020-02-29 14:36:32 -0800

    made a bag of words to feed data to model (hot_in_coded)

--- Dr.Chatbot.py
+++ Dr.Chatbot.py
@@ -15,13 +15,39 @@ print(data["intents"])
 
 words = []
 labels = []
-docs = []
+docs_x = []
+docs_y = []
 
 for intent in data["intents"]:
     for patter in intent["pattern"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs.append(pattern)
+        docs_x.append(pattern)
+        docs_y.append()
 
         if intent["tag"] not in label:
             labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words]
+words = sorted(List(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(classes))]
+
+for x, doc in enumerate(doc_x):
+    bag = []
+
+    wrds = [stemmer.stem(w) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
